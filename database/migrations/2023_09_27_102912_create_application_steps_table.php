36f35c2dc01a0b297de27b72d5d5b52cea9c38dd
Aydin Pramasta <aydin.pram@gmail.com>
2023-09-27 11:53:11 +0700

    refactor: seeders

--- database/migrations/2023_09_27_102912_create_application_steps_table.php
+++ database/migrations/2023_09_27_102912_create_application_steps_table.php
@@ -15,8 +15,8 @@ return new class extends Migration {
             $table->string('status');
             $table->foreignId('application_id')->constrained();
             $table->foreignId('step_id')->constrained();
-            $table->foreignId('created_by')->constrained('users');
-            $table->foreignId('updated_by')->constrained('users');
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
             $table->foreignId('deleted_by')->nullable()->constrained('users');
             $table->timestamps();
             $table->softDeletes();
--- database/migrations/2023_09_27_104421_add_current_application_step_id_to_applications_table.php
+++ database/migrations/2023_09_27_104421_add_current_application_step_id_to_applications_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::table('applications', function (Blueprint $table) {
             $table->foreignId('current_application_step_id')
                 ->unique()
+                ->nullable()
                 ->constrained('application_steps');
         });
     }
