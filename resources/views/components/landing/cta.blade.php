baa7126960120fdbf3ecb22f0e3aa26d2a94e02f
Aydin Pramasta <aydin.pram@gmail.com>
2023-09-27 14:26:19 +0700

    fix: background image positioning

--- resources/views/components/landing/cta.blade.php
+++ resources/views/components/landing/cta.blade.php
@@ -1,7 +1,8 @@
 <section class="cta">
-    <div class="cta__container">
-        <img class="cta__illustration" alt="CTA Illustration" src="{{ asset('/assets/images/circle_cta.svg') }}"/>
-        <h2 class="cta__title">Jangan Tunggu Lagi, Bergabunglah dengan SEVIMA!</h2>
-        <x-link href="{{ route('jobs') }}" class="cta__button">Lamar Sekarang</x-link>
+    <div class="cta__wrapper">
+        <div class="cta__container" style="background-image: url({{ asset('/assets/images/circle_cta.svg') }})">
+            <h2 class="cta__title">Jangan Tunggu Lagi, Bergabunglah dengan SEVIMA!</h2>
+            <x-link href="{{ route('jobs') }}" class="cta__button">Lamar Sekarang</x-link>
+        </div>
     </div>
 </section>
