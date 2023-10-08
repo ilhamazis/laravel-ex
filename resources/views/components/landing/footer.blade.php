cc018aac38a7d4ce6b7e2765aedcbd35bed1d153
Aydin Pramasta <aydin.pram@gmail.com>
2023-10-08 19:09:24 +0700

    fix: style conflict on footer

--- resources/views/components/landing/footer.blade.php
+++ resources/views/components/landing/footer.blade.php
@@ -1,59 +1,59 @@
-<footer class="footer">
+<footer class="custom__footer">
     <div>
-        <div class="footer__logo">
+        <div class="custom__footer-logo">
             <img src="{{ asset('/assets/images/logo_sevima.svg') }}" alt="Sevima Logo"/>
-            <span class="footer__logo-text">Career</span>
+            <span class="custom__footer-logo-text">Career</span>
         </div>
 
-        <p class="footer__description">
+        <p class="custom__footer-description">
             Develop Your Career at SEVIMA: Opportunity, Innovation, Success.
         </p>
 
-        <p class="footer__copyright">
+        <p class="custom__footer-copyright">
             © 2023 SEVIMA. All Right Reserved
         </p>
     </div>
 
-    <div class="footer__links">
-        <div class="footer__links-group">
-            <h6 class="footer__links-title">Produk</h6>
-            <ul class="footer__links-list">
-                <li class="footer__links-item">
+    <div class="custom__footer-links">
+        <div class="custom__footer-links-group">
+            <h6 class="custom__footer-links-title">Produk</h6>
+            <ul class="custom__footer-links-list">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Edlink
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Maukuliah
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Siakad Cloud
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Akreditasi Cloud
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Profeeder
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         GoFeeder
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Karirlink
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Sevima Pay
                     </a>
@@ -61,25 +61,25 @@
             </ul>
         </div>
 
-        <div class="footer__links-group">
-            <h6 class="footer__links-title">Perusahaan</h6>
-            <ul class="footer__links-list">
-                <li class="footer__links-item">
+        <div class="custom__footer-links-group">
+            <h6 class="custom__footer-links-title">Perusahaan</h6>
+            <ul class="custom__footer-links-list">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Tentang SEVIMA
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Event
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Berita
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Perguruan Tinggi
                     </a>
@@ -87,20 +87,20 @@
             </ul>
         </div>
 
-        <div class="footer__links-group">
-            <h6 class="footer__links-title">Legal</h6>
-            <ul class="footer__links-list">
-                <li class="footer__links-item">
+        <div class="custom__footer-links-group">
+            <h6 class="custom__footer-links-title">Legal</h6>
+            <ul class="custom__footer-links-list">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Privacy Policy
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Terms & Conditions
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Sitemap
                     </a>
@@ -108,30 +108,30 @@
             </ul>
         </div>
 
-        <div class="footer__links-group">
-            <h6 class="footer__links-title">Ikuti Sevima</h6>
-            <ul class="footer__links-list">
-                <li class="footer__links-item">
+        <div class="custom__footer-links-group">
+            <h6 class="custom__footer-links-title">Ikuti Sevima</h6>
+            <ul class="custom__footer-links-list">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Facebook
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Instagram
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Twitter
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Youtube
                     </a>
                 </li>
-                <li class="footer__links-item">
+                <li class="custom__footer-links-item">
                     <a href="#">
                         Linkedin
                     </a>
