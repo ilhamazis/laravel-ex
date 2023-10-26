2c5c8dc9c4f900840cd61938c695f0174ab2c630
Aydin Pramasta <aydin.pram@gmail.com>
2023-10-26 09:23:11 +0700

    refactor: add & use card nav component on job

--- resources/views/layouts/job.blade.php
+++ resources/views/layouts/job.blade.php
@@ -12,22 +12,7 @@
         <x-cms.job.header-detail :job="$job"/>
 
         <div class="card">
-            <div class="card__body">
-                <nav class="nav-tab">
-                    <ul class="nav-tab__wrapper">
-                        <li @class(['nav-tab__item', 'active' => request()->routeIs('managements.jobs.show')])>
-                            <x-link :href="route('managements.jobs.show', $job)">Deskripsi</x-link>
-                        </li>
-                        @can(\App\Enums\PermissionEnum::VIEW_APPLICATION->value)
-                            <li @class([
-                                'nav-tab__item', 'active' => request()->routeIs('managements.jobs.applications.index'),
-                            ])>
-                                <x-link :href="route('managements.jobs.applications.index', $job)">List Pelamar</x-link>
-                            </li>
-                        @endcan
-                    </ul>
-                </nav>
-            </div>
+            <x-cms.job.card-navigation/>
 
             {{ $slot }}
         </div>
