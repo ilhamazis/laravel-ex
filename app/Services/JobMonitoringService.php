ec26bdcda62a1a011ba1ccd2be6c22d1a5667186
Aydin Pramasta <aydin.pram@gmail.com>
2023-10-31 13:49:22 +0700

    refactor: dashboard items

--- app/Services/JobMonitoringService.php
+++ app/Services/JobMonitoringService.php
@@ -11,67 +11,31 @@ use Illuminate\Support\Facades\DB;
 
 class JobMonitoringService
 {
-    public function getActiveJobsCount(): int
+    public function getActiveJobsCount(): string
     {
         return $this->numberFormat(Job::query()->isActive()->count());
     }
 
-    public function getApplicationsCount(): int
-    {
-        return $this->numberFormat(Application::query()->count());
-    }
-
-    public function getApplicationsCountFromRange(Carbon $from, Carbon $to): int
+    public function getJobsThatDoesntHaveApplications(): string
     {
         return $this->numberFormat(
-            Application::query()
-                ->whereBetween('created_at', [$from, $to])
+            Job::query()
+                ->whereDoesntHave('applications')
                 ->count()
         );
     }
 
-    /**
-     * @param int $year
-     * @return array<string, int>
-     */
-    public function getApplicationsCountPerMonth(int $year): array
+    public function getApplicationsByJob(): array
     {
-        $applicationsPerMonthCount = Application::query()
-            ->select(DB::raw("DATE_TRUNC('MONTH', created_at) as month"), DB::raw("COUNT(*) as total"))
-            ->whereBetween('created_at', [now()->setYear($year)->startOfYear(), now()->setYear($year)->endOfYear()])
-            ->groupBy('month')
-            ->orderBy('month')
+        $applicationsByJob = Job::query()
+            ->withCount('applications')
+            ->isActive()
+            ->latest('updated_at')
             ->get();
 
-        /**
-         * base months, value is:
-         * [
-         *     'Jan 2023' => 0,
-         *     'Feb 2023' => 0,
-         *     etc...
-         *     'Oct 2023' => null,
-         *     etc...
-         * ]
-         */
-        $months = collect($this->getMonthsFromYear($year))
-            ->mapWithKeys(function (string $item, int $key) use ($year) {
-                if (now()->year === $year) {
-                    return [$item => $key < now()->month ? 0 : null];
-                }
-
-                return [$item => 0];
-            });
-
-        // count per month in DB, structure is same as base months
-        $applicationsPerMonthCount = $applicationsPerMonthCount->mapWithKeys(function ($item) {
-            // key is month & date, example: Jan 2023
-            $key = Carbon::parse($item->month)->format('M Y');
-
-            return [$key => $item->total];
-        });
-
-        // merge base months and count per month
-        return $months->merge($applicationsPerMonthCount)->toArray();
+        return $applicationsByJob
+            ->mapWithKeys(fn(Job $job) => [$job->title => $job->applications_count])
+            ->toArray();
     }
 
     /**
@@ -86,25 +50,8 @@ class JobMonitoringService
         ];
     }
 
-    private function numberFormat(int $value): int
+    private function numberFormat(int $value): string
     {
         return number_format($value, decimal_separator: ',', thousands_separator: '.');
-    }
-
-    /**
-     * @param int $year
-     * @return array<string>
-     */
-    private function getMonthsFromYear(int $year): array
-    {
-        $start = now()->setYear($year)->startOfYear();
-        $end = now()->setYear($year)->endOfYear();
-
-        $months = [];
-        foreach (CarbonPeriod::create($start, '1 month', $end) as $month) {
-            $months[] = $month->format('M Y');
-        }
-
-        return $months;
     }
 }
