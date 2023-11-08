a967c6ed135ca6cc68dd3068bba702872c11add7
Aydin Pramasta <aydin.pram@gmail.com>
2023-11-08 09:56:08 +0700

    refactor: badges on job detail & apply form

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -21,10 +21,23 @@
         </div>
 
         <div class="jobs-detail__badges">
-            <span class="custom__badge custom__badge-outline">{{ $job->type }}</span>
+            <div class="custom__badge custom__badge-outline">
+                <svg width="16" height="16" viewBox="0 0 20 20"
+                     fill="none">
+                    <path
+                        d="M16.668 5.83333H3.33464C2.41416 5.83333 1.66797 6.57952 1.66797 7.49999V15.8333C1.66797 16.7538 2.41416 17.5 3.33464 17.5H16.668C17.5884 17.5 18.3346 16.7538 18.3346 15.8333V7.49999C18.3346 6.57952 17.5884 5.83333 16.668 5.83333Z"
+                        stroke="#3955A4" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round"/>
+                    <path
+                        d="M13.3346 17.5V4.16667C13.3346 3.72464 13.159 3.30072 12.8465 2.98816C12.5339 2.67559 12.11 2.5 11.668 2.5H8.33464C7.89261 2.5 7.46869 2.67559 7.15612 2.98816C6.84356 3.30072 6.66797 3.72464 6.66797 4.16667V17.5"
+                        stroke="#3955A4" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round"/>
+                </svg>
+                {{ $job->type }}
+            </div>
             <div class="custom__badge custom__badge-secondary">
                 <span class="icon icon-clipboard-document-list-solid"></span>
-                {{ $job->quota }} kuota
+                {{ $job->quota }} Kuota Tersisa
             </div>
         </div>
 
