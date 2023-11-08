6ede106c82d77ae8812626fe99f88fd36913e317
Aydin Pramasta <aydin.pram@gmail.com>
2023-11-08 14:36:35 +0700

    refactor: applicant factory refer to migration

--- database/factories/ApplicantFactory.php
+++ database/factories/ApplicantFactory.php
@@ -18,20 +18,22 @@ class ApplicantFactory extends Factory
     public function definition(): array
     {
         return [
+            'photo' => 'dummy.png',
             'name' => fake()->name(),
             'nik' => fake()->numberBetween(1000000000, 2147483647),
-            'email' => fake()->safeEmail(),
-            'telephone' => fake()->numberBetween(8000000000, 8999999999),
             'place_of_birth' => fake()->city(),
             'date_of_birth' => fake()->date(),
-            'is_married' => fake()->boolean(),
             'gender' => collect(['Laki-laki', 'Perempuan'])->random(),
+            'is_married' => fake()->boolean(),
             'address' => fake()->address(),
+            'email' => fake()->safeEmail(),
+            'telephone' => fake()->numberBetween(8000000000, 8999999999),
             'education' => collect(['S3', 'S2', 'S1', 'D4', 'D3', 'D2', 'D1', 'SMK', 'SMA'])->random(),
             'school' => fake()->company(),
             'faculty' => fake()->words(asText: true),
             'major' => fake()->words(asText: true),
             'experience' => collect(ApplicationExperienceEnum::values())->random(),
+            'linkedin_url' => fake()->url(),
         ];
     }
 }
