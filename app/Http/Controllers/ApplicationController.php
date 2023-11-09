29d3e01141e06ba1b98bc97136768e9cd7e96155
Aydin Pramasta <aydin.pram@gmail.com>
2023-11-09 14:27:07 +0700

    fix: add middleware to application resource

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\PermissionEnum;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('can:' . PermissionEnum::VIEW_APPLICATION->value)->only(['index']);
+    }
+    
     public function index(): View
     {
         return view('managements.applications.index');
