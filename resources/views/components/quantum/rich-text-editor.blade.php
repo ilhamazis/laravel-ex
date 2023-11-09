ef91dff3f603fc4a18af9abb8771520d4ee7fe4b
Aydin Pramasta <aydin.pram@gmail.com>
2023-11-09 09:11:34 +0700

    refactor: use sanitize global function on rich text editor

--- resources/views/components/quantum/rich-text-editor.blade.php
+++ resources/views/components/quantum/rich-text-editor.blade.php
@@ -4,23 +4,20 @@
     $quillId = $id . 'Quill';
 @endphp
 
-<x-quantum.quill id="{{ $quillId }}"></x-quantum.quill>
+<x-quantum.quill id="{{ $quillId }}" x-init="sanitize($el, @js($value))"></x-quantum.quill>
 
 <textarea id="{{ $id }}" name="{{ $name }}"
+          x-init="sanitize($el, @js($value))"
           style="display: none"></textarea>
-
-@once
-    @push('scripts')
-        <script type="text/javascript" src="{{ asset('dompurify-3.0.6/purify.min.js') }}"></script>
-    @endpush
-@endonce
 
 @push('custom-scripts')
     <script>
-        document.getElementById(@js($quillId))
-            .querySelector('.ql-editor').innerHTML = DOMPurify.sanitize(@js($value));
-
-        document.getElementById(@js($id)).innerHTML = DOMPurify.sanitize(@js($value));
+        document.addEventListener('DOMContentLoaded', () => {
+            if (typeof Livewire === 'undefined') {
+                sanitize(document.getElementById(@js($quillId)).querySelector('.ql-editor'), @js($value));
+                sanitize(document.getElementById(@js($id)), @js($value));
+            }
+        });
 
         document.getElementById(@js($quillId))
             .querySelector('.ql-editor')
